e8ac4188bb7c3eb05fa5236b44323f4a5f5592b0
Erick Santiago Mahecha Tafur <santiagomahecha2328@gmail.com>
2026-01-06 13:22:28 -0500

    Update GalleryView.tsx

--- src/components/gallery/GalleryView.tsx
+++ src/components/gallery/GalleryView.tsx
@@ -13,7 +13,7 @@ export default function GalleryView() {
           sizes="(max-width: 560px) 100vw, 50vw"
           key={index}
           radius="m"
-          aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "9 / 16"}
+          aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "3 / 4"}
           src={image.src}
           alt={image.alt}
         />
