ff6d5cccc689d2cf8dee9e2a27a43f8e503137eb
Erick Santiago Mahecha Tafur <santiagomahecha2328@gmail.com>
2026-01-06 23:19:55 -0500

    Update content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -289,7 +289,7 @@ technical: {
 const blog: Blog = {
   path: "/blog",
   label: "Blog",
-  title: "Writing about design and tech...",
+  title: "Insights & Setup",
   description: `Read what ${person.name} has been up to recently`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
