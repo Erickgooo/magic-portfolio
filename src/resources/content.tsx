160c6c772b342c2476b3ab5d29132282dfdf31a0
Erick Santiago Mahecha Tafur <santiagomahecha2328@gmail.com>
2026-06-07 21:55:01 -0500

    feat: change featured work to QMS ArquiExpo App on Home page

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -49,14 +49,14 @@ const home: Home = {
     display: true,
     title: (
       <Row gap="12" vertical="center">
-        <strong className="ml-4">LeadBot AI</strong>{" "}
+        <strong className="ml-4">QMS ArquiExpo App</strong>{" "}
         <Line background="brand-alpha-strong" vert height="20" />
         <Text marginRight="4" onBackground="brand-medium">
           Featured work
         </Text>
       </Row>
     ),
-    href: "/work/leadbot-ai",
+    href: "/work/quick-metal-shop-arquiexpo",
   },
   subline: (
     <>
